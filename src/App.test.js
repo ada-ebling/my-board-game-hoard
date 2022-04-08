c04519443deb8be1b62bc454381d538d46399f8e
Ada Ebling <ada.ebling@gmail.com>
2022-04-08 17:11:26 -0600

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from './App';
-
-test('renders learn react link', async () => {
-  render(<App />);
-  const fetchButton = screen.getByText('Fetch My Data!');
-  userEvent.click(fetchButton);
-});
